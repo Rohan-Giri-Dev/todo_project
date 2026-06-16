525c9e4bebbaa6250bc49d2e43a7cebb103edddc
Rohan Giri <giri.dev.rohan@gmail.com>
2026-06-16 21:03:20 +0530

    get method of subscription

--- app/api/subscription/route.ts
+++ app/api/subscription/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import {auth} from '@clerk/nextjs/server'
 import { prisma } from "@/lib/prisma";
+import { Select } from "radix-ui";
 
 export async function POST(){
     const {userId} = await auth()
@@ -54,12 +55,71 @@ export async function POST(){
 }
 
 
-export async function GET(){}
+export async function GET(){
+    const {userId} = await auth()
+
+     if (!userId) {
+    return NextResponse.json(
+      { error: "Unauthorized" },
+      { status: 401 }
+    );
+    }
+
+    try {
+        const user =  await prisma.user.findUnique(
+            {where:{id: userId},
+         select: { 
+            isSubscribed: true,
+            subscriptionEnds: true,
+         }}, 
+        )
+
+         if(!user) {
+            return NextResponse.json(
+                { error: "User not found" },
+                { status: 401 }
+            );  
+        }
+
+        const now = new Date();
+
+        if(user.subscriptionEnds && user.subscriptionEnds < now){
+            await prisma.user.update({
+                where: {id: userId},
+                data: {
+                    isSubscribed: false,
+                    subscriptionEnds: null,
+                }
+            });
+
+            return NextResponse.json({
+                isSubscribed: false,
+                subscriptionEnds: null
+
+            })
+        }
+
+         return NextResponse.json({
+                isSubscribed: user.isSubscribed,
+                subscriptionEnds: user.subscriptionEnds
+
+            })
+
+    } catch (error) {
+        console.log('Error updating subscription', error);
+        return NextResponse.json({
+            error: "Internal server error"
+        })
+    }
+}
 
 
 
 
 /*
+
+1) 
+we are using auth() for getting the userId because here we are using clerk for the aunthication 
 Common values inside auth() are:
 
 const {
